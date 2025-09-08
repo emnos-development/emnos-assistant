ffa39fddb1eb3c257bfed637c7b0b5a040ab0d95
Meriem Khamari <meriem.khamari@emnos.com>
2025-09-08 15:55:32 +0200

    OPS-1140: Formatting

--- emnos-tools/create-dockerfile.sh
+++ emnos-tools/create-dockerfile.sh
@@ -4,19 +4,22 @@ cat << EOF >> ./emnos-tools/dockerfile
 
 FROM $LATEST_BASE_IMAGE:$LATEST_BASE_IMAGE_TAG
 
+RUN mkdir -p /opt/emnosapp/private-gpt \
+    && chown -R emnos:emnos /opt/emnosapp/private-gpt
+
 USER emnos
 ENV HOME=/home/emnos
-ENV PATH="$HOME/.local/bin:$PATH"
+ENV PATH="/home/emnos/.local/bin:$PATH"
 WORKDIR /opt/emnosapp/private-gpt
 
 SHELL ["/bin/bash", "-o", "pipefail", "-c"]
-RUN curl -sSL https://install.python-poetry.org | python3 -
 
 COPY --chown=emnos:emnos . /opt/emnosapp/private-gpt
 
 COPY --chown=emnos:emnos ./local_data/private_gpt/qdrant /opt/emnosapp/private-gpt/local_data/qdrant
 
-RUN poetry install --extras "llms-gemini embeddings-gemini ui vector-stores-qdrant" --no-interaction --no-ansi \
+RUN curl -sSL https://install.python-poetry.org | python3 - \
+    && poetry install --extras "llms-gemini embeddings-gemini ui vector-stores-qdrant" --no-interaction --no-ansi \
     && pip install -e .
 
 EXPOSE 8080
