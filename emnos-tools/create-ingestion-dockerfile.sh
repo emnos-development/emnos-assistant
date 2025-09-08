1011cd8ac7c1acb86035fb8c233d486787314e42
Meriem Khamari <meriem.khamari@emnos.com>
2025-09-08 15:48:20 +0200

    OPS-1140: Debug

--- emnos-tools/create-ingestion-dockerfile.sh
+++ emnos-tools/create-ingestion-dockerfile.sh
@@ -9,7 +9,7 @@ RUN mkdir -p /opt/emnosapp/private-gpt \
 
 USER emnos
 ENV HOME=/home/emnos
-ENV PATH="$HOME/.local/bin:$PATH"
+ENV PATH="/home/emnos/.local/bin:$PATH"
 WORKDIR /opt/emnosapp/private-gpt
 
 SHELL ["/bin/bash", "-o", "pipefail", "-c"]
@@ -17,7 +17,7 @@ SHELL ["/bin/bash", "-o", "pipefail", "-c"]
 COPY --chown=emnos:emnos . /opt/emnosapp/private-gpt
 
 RUN curl -sSL https://install.python-poetry.org | python3 - \
-    && $HOME/.local/bin/poetry install --extras "llms-gemini embeddings-gemini ui vector-stores-qdrant" --no-interaction --no-ansi \
+    && poetry install --extras "llms-gemini embeddings-gemini ui vector-stores-qdrant" --no-interaction --no-ansi \
     && pip install -e .
 
 ENTRYPOINT ["bash", "-c", "make wipe && make ingest $INGEST_FOLDER"]
