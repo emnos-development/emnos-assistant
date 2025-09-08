2f7d4622950206a0b95ddcebf73ed73c5eec0c2a
Meriem Khamari <meriem.khamari@emnos.com>
2025-09-08 12:37:19 +0200

    OPS-1140: Optimize startup time

---
+++ emnos-tools/create-dockerfile.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+cat << EOF >> dockerfile
+
+FROM $LATEST_BASE_IMAGE:$LATEST_BASE_IMAGE_TAG
+
+USER emnos
+ENV HOME=/home/emnos
+ENV PATH="$HOME/.local/bin:$PATH"
+WORKDIR /opt/emnosapp/private-gpt
+
+SHELL ["/bin/bash", "-o", "pipefail", "-c"]
+RUN curl -sSL https://install.python-poetry.org | python3 -
+
+COPY --chown=emnos:emnos . /opt/emnosapp/private-gpt
+
+COPY --chown=emnos:emnos ./local_data/private_gpt/qdrant /opt/emnosapp/private-gpt/local_data/qdrant
+
+RUN poetry install --extras "llms-gemini embeddings-gemini ui vector-stores-qdrant" --no-interaction --no-ansi \
+    && pip install  --no-cache-dir -e .
+
+EXPOSE 8080
+ENTRYPOINT ["bash", "-c", "GPT_PROFILES=gemini make run"]
+
+EOF
+
+exit 0
